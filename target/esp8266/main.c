81374c48078a2fd1b8875f4c3ed40f5dbffcfa05
Ivan Grokhotkov <igrokhotkov@spbtv.com>
2014-10-07 14:50:31 +0400

    Mark functions with ICACHE_FLASH_ATTR

--- target/esp8266/main.c
+++ target/esp8266/main.c
@@ -7,12 +7,12 @@
 
 uart_t* uart0;
 
-void uart_echo(char c)
+void ICACHE_FLASH_ATTR uart_echo(char c)
 {
 	uart0_transmit(&c, 1);
 }
 
-void user_init(void)
+void ICACHE_FLASH_ATTR user_init(void)
 {
     uart0 = uart0_init(9600, &uart_echo);
 }
--- target/esp8266/uart.c
+++ target/esp8266/uart.c
@@ -11,7 +11,7 @@ struct _uart
     uart_rx_handler_t rx_handler;
 };
 
-void uart0_rx_handler(uart_t* uart)
+void ICACHE_FLASH_ATTR uart0_rx_handler(uart_t* uart)
 {
     while(true)
     {
@@ -27,7 +27,7 @@ void uart0_rx_handler(uart_t* uart)
     }
 }
 
-void uart0_wait_for_tx_fifo(size_t size_needed)
+void ICACHE_FLASH_ATTR uart0_wait_for_tx_fifo(size_t size_needed)
 {
     while (true)
     {
@@ -37,7 +37,7 @@ void uart0_wait_for_tx_fifo(size_t size_needed)
     }
 }
 
-void uart0_transmit(const char* buf, size_t size)
+void ICACHE_FLASH_ATTR uart0_transmit(const char* buf, size_t size)
 {
     while (size)
     {
@@ -49,25 +49,27 @@ void uart0_transmit(const char* buf, size_t size)
     }
 }
 
-void uart0_flush(uart_t* uart)
+void ICACHE_FLASH_ATTR uart0_flush(uart_t* uart)
 {
     SET_PERI_REG_MASK(UART_CONF0(0), UART_RXFIFO_RST | UART_TXFIFO_RST);
     CLEAR_PERI_REG_MASK(UART_CONF0(0), UART_RXFIFO_RST | UART_TXFIFO_RST);
 }
 
-void uart0_interrupt_enable(uart_t* uart)
+void ICACHE_FLASH_ATTR uart0_interrupt_enable(uart_t* uart)
 {
     WRITE_PERI_REG(UART_INT_CLR(0), 0x1ff);
     ETS_UART_INTR_ATTACH(&uart0_rx_handler, uart);
     SET_PERI_REG_MASK(UART_INT_ENA(0), UART_RXFIFO_FULL_INT_ENA);
+    ETS_UART_INTR_ENABLE();
 }
 
-void uart0_interrupt_disable(uart_t* uart)
+void ICACHE_FLASH_ATTR uart0_interrupt_disable(uart_t* uart)
 {
-    SET_PERI_REG_MASK(UART_INT_ENA(0), UART_RXFIFO_FULL_INT_ENA);
+    SET_PERI_REG_MASK(UART_INT_ENA(0), 0);
+    ETS_UART_INTR_DISABLE();
 }
 
-uart_t* uart0_init(int baud_rate, uart_rx_handler_t rx_handler)
+uart_t* ICACHE_FLASH_ATTR uart0_init(int baud_rate, uart_rx_handler_t rx_handler)
 {
     uart_t* uart = (uart_t*) os_malloc(sizeof(uart_t));
     uart->baud_rate = baud_rate;
@@ -88,7 +90,7 @@ uart_t* uart0_init(int baud_rate, uart_rx_handler_t rx_handler)
 }
 
 
-void uart0_uninit(uart_t* uart)
+void ICACHE_FLASH_ATTR uart0_uninit(uart_t* uart)
 {
     uart0_interrupt_disable(uart);
     // TODO: revert pin functions
