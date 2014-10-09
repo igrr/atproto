a1357ce831b4203bb28299ccf559efbaca3bf234
Ivan Grokhotkov <igrokhotkov@gmail.com>
2014-10-09 13:30:28 +0400

    Fix indentation

--- target/esp8266/main.c
+++ target/esp8266/main.c
@@ -21,12 +21,12 @@ os_event_t command_queue[COMMAND_QUEUE_SIZE];
 
 void ICACHE_FLASH_ATTR uart_echo(char c)
 {
-	uart0_transmit_char(uart0, c);
+    uart0_transmit_char(uart0, c);
 }
 
 void ICACHE_FLASH_ATTR rx_dce_cb(char c)
 {
-	dce_handle_input(dce, &c, 1);
+    dce_handle_input(dce, &c, 1);
 }
 
 void ICACHE_FLASH_ATTR target_dce_request_process_command_line(dce_t* dce)
@@ -36,7 +36,7 @@ void ICACHE_FLASH_ATTR target_dce_request_process_command_line(dce_t* dce)
 
 void command_task(os_event_t *events)
 {
-	dce_process_command_line(dce);
+    dce_process_command_line(dce);
 }
 
 void ICACHE_FLASH_ATTR target_dce_transmit(const char* data, size_t size)
@@ -51,7 +51,7 @@ void ICACHE_FLASH_ATTR target_dce_reset()
 
 void ICACHE_FLASH_ATTR target_dce_assert(const char* message)
 {
-	uart0_transmit(uart0, "\r\n########\r\n", 12);
+    uart0_transmit(uart0, "\r\n########\r\n", 12);
     uart0_transmit(uart0, message, os_strlen(message));
     uart0_transmit(uart0, "\r\n########\r\n", 12);
     uart0_wait_for_transmit(uart0);
@@ -75,14 +75,14 @@ void ICACHE_FLASH_ATTR config_init(void)
 void ICACHE_FLASH_ATTR user_init(void)
 {
     config_init();
-	dce = dce_init(256);
+    dce = dce_init(256);
     uart0 = uart0_init(config->baud_rate, &rx_dce_cb);
     dce_register_interface_commands(dce, uart0);
     dce_register_info_commands(dce);
     uart_disable_debug();
     system_os_task( command_task,
-    				COMMAND_TASK_PRIORITY,
-    				command_queue,
-    				COMMAND_QUEUE_SIZE);
+                    COMMAND_TASK_PRIORITY,
+                    command_queue,
+                    COMMAND_QUEUE_SIZE);
 }
 
