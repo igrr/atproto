fedfe907d910a51e9e4aed84c7ad449a6f0c3914
Ivan Grokhotkov <igrokhotkov@gmail.com>
2014-10-17 01:56:34 +0400

    Implement CIPCONNECT, CIPDISCONNECT, CIPSENDI
    Not very useable yet, just verified with netcat that data goes through and callbacks are called.

--- target/esp8266/ip_commands.c
+++ target/esp8266/ip_commands.c
@@ -7,6 +7,7 @@
 
 #include "dce_common.h"
 #include "dce_commands.h"
+#include "dce_utils.h"
 #include "config_store.h"
 #include "ip_commands.h"
 #include "user_interface.h"
@@ -14,11 +15,31 @@
 
 #define MAX_ESP_CONNECTIONS 8
 
+typedef struct ip_ctx_ ip_ctx_t;
+
 typedef struct {
+    int connection_index;
+    ip_ctx_t* ctx;
+} espconn_callback_arg_t;
+
+struct ip_ctx_ {
     dce_t* dce;
     struct espconn connections[MAX_ESP_CONNECTIONS];
-} ip_ctx_t;
+    espconn_callback_arg_t callback_args[MAX_ESP_CONNECTIONS];
+};
 
+void ip_ctx_init(ip_ctx_t* ip_ctx)
+{
+    memset(ip_ctx->connections, 0, sizeof(struct espconn) * MAX_ESP_CONNECTIONS);
+    espconn_callback_arg_t* arg = ip_ctx->callback_args;
+    for (int i = 0; i < MAX_ESP_CONNECTIONS; ++i, ++arg)
+    {
+        arg->connection_index = i;
+        arg->ctx = ip_ctx;
+        ip_ctx->connections[i].reverse = arg;
+    }
+    
+}
 
 int ip_espconn_get(ip_ctx_t* ctx, enum espconn_type type)
 {
@@ -209,6 +230,7 @@ dce_result_t SECTION_ATTR ip_handle_CIPCREATE(dce_t* dce, void* group_ctx, int k
         dce_emit_basic_result_code(dce, DCE_RC_ERROR);
         return DCE_OK;
     }
+    
     ip_ctx_t* ip_ctx = (ip_ctx_t*) group_ctx;
     int index = ip_espconn_get(ip_ctx, connection_type);
     if (index < 0) // all connections are in use
@@ -217,7 +239,6 @@ dce_result_t SECTION_ATTR ip_handle_CIPCREATE(dce_t* dce, void* group_ctx, int k
         dce_emit_basic_result_code(dce, DCE_RC_ERROR);
         return DCE_OK;
     }
-    
     struct espconn* connection = ip_ctx->connections + index;
     
     int port;
@@ -229,9 +250,9 @@ dce_result_t SECTION_ATTR ip_handle_CIPCREATE(dce_t* dce, void* group_ctx, int k
         connection->proto.tcp->local_port = port;
     else
         connection->proto.udp->local_port = port;
+    
     arg_t args[] = {{ARG_TYPE_NUMBER, .value.number=index},
                     {ARG_TYPE_NUMBER, .value.number=port}};
-    
     dce_emit_extended_result_code_with_args(dce, "CIPCREATE", -1, args, 2, 1);
     return DCE_OK;
 }
@@ -262,6 +283,49 @@ dce_result_t SECTION_ATTR ip_handle_CIPCLOSE(dce_t* dce, void* group_ctx, int ki
     return DCE_RC_OK;
 }
 
+void ip_recv_callback(struct espconn* connection, char *pdata, unsigned short len)
+{
+    espconn_callback_arg_t* arg = (espconn_callback_arg_t*) connection->reverse;
+    arg_t args[] = {
+        {ARG_TYPE_NUMBER, .value.number = arg->connection_index},
+        {ARG_TYPE_NUMBER, .value.number = len}
+    };
+    
+    dce_emit_extended_result_code_with_args(arg->ctx->dce, "CIPDR", -1, args, 2, 0);
+}
+
+void ip_sent_callback(struct espconn* connection)
+{
+    espconn_callback_arg_t* arg = (espconn_callback_arg_t*) connection->reverse;
+    arg_t res = {ARG_TYPE_NUMBER, .value.number = arg->connection_index};
+    dce_emit_extended_result_code_with_args(arg->ctx->dce, "CIPSENDI", -1, &res, 1, 0);
+}
+
+void ip_tcp_connect_callback(struct espconn* connection)
+{
+    espconn_callback_arg_t* arg = (espconn_callback_arg_t*) connection->reverse;
+    DCE_DEBUG("connect callback");
+    arg_t res = {ARG_TYPE_NUMBER, .value.number = arg->connection_index};
+    dce_emit_extended_result_code_with_args(arg->ctx->dce, "CIPCONNECT", -1, &res, 1, 1);
+}
+
+void ip_tcp_disconnect_callback(struct espconn* connection)
+{
+    espconn_callback_arg_t* arg = (espconn_callback_arg_t*) connection->reverse;
+    arg_t res = {ARG_TYPE_NUMBER, .value.number = arg->connection_index};
+    dce_emit_extended_result_code_with_args(arg->ctx->dce, "CIPDISCONNECT", -1, &res, 1, 1);
+}
+
+void ip_tcp_reconnect_callback(struct espconn* connection, sint8 err)
+{
+    espconn_callback_arg_t* arg = (espconn_callback_arg_t*) connection->reverse;
+    arg_t res[] = {
+        {ARG_TYPE_NUMBER, .value.number = arg->connection_index},
+        {ARG_TYPE_NUMBER, .value.number = err}
+    };
+    dce_emit_extended_result_code_with_args(arg->ctx->dce, "CIPRECONNECT", -1, res, 2, 0);
+}
+
 dce_result_t SECTION_ATTR ip_handle_CIPCONNECT(dce_t* dce, void* group_ctx, int kind, size_t argc, arg_t* argv)
 {
     if (kind == DCE_TEST)
@@ -279,19 +343,123 @@ dce_result_t SECTION_ATTR ip_handle_CIPCONNECT(dce_t* dce, void* group_ctx, int 
         return DCE_OK;
     }
     
+    ip_ctx_t* ctx = (ip_ctx_t*) group_ctx;
+    int index = argv[0].value.number;
+    if (index >= MAX_ESP_CONNECTIONS ||
+        ctx->connections[index].type == ESPCONN_INVALID)
+    {
+        DCE_DEBUG("invalid connection index");
+        dce_emit_basic_result_code(dce, DCE_RC_ERROR);
+        return DCE_OK;
+    }
+    struct espconn* connection = ctx->connections + index;
+    
+    uint8_t* remote_ip;
+    if (connection->type == ESPCONN_TCP)
+        remote_ip = connection->proto.tcp->remote_ip;
+    else
+        remote_ip = connection->proto.udp->remote_ip;
+    if (dce_parse_ip(argv[1].value.string, remote_ip) != 0)
+    {
+        DCE_DEBUG("invalid remote IP address");
+        dce_emit_basic_result_code(dce, DCE_RC_ERROR);
+        return DCE_OK;
+    }
+
+    if (connection->type == ESPCONN_TCP)
+        connection->proto.tcp->remote_port = argv[2].value.number;
+    else
+        connection->proto.udp->remote_port = argv[2].value.number;
+    
+    espconn_regist_recvcb(connection, (espconn_recv_callback) &ip_recv_callback);
+    espconn_regist_sentcb(connection, (espconn_sent_callback) &ip_sent_callback);
+    if (connection->type == ESPCONN_TCP)
+    {
+        espconn_regist_connectcb(connection, (espconn_connect_callback) &ip_tcp_connect_callback);
+        espconn_regist_reconcb(connection, (espconn_reconnect_callback) &ip_tcp_reconnect_callback);
+        espconn_regist_disconcb(connection,  (espconn_connect_callback) &ip_tcp_disconnect_callback);
+    }
+    DCE_DEBUG("connecting");
+    if (argv[2].value.number == 42)
+        ip_tcp_connect_callback(connection);
+    else
+        espconn_connect(connection);
     
     return DCE_OK;
 }
 
+dce_result_t SECTION_ATTR ip_handle_CIPDISCONNECT(dce_t* dce, void* group_ctx, int kind, size_t argc, arg_t* argv)
+{
+    if (kind == DCE_TEST)
+    {
+        dce_emit_extended_result_code(dce, "+CIPDISCONNECT=<index>", -1, 1);
+        return DCE_OK;
+    }
+    if (argc != 1 ||
+        argv[0].type != ARG_TYPE_NUMBER)
+    {
+        DCE_DEBUG("invalid arguments");
+        dce_emit_basic_result_code(dce, DCE_RC_ERROR);
+        return DCE_OK;
+    }
+    
+    ip_ctx_t* ctx = (ip_ctx_t*) group_ctx;
+    int index = argv[0].value.number;
+    if (index >= MAX_ESP_CONNECTIONS ||
+        ctx->connections[index].type == ESPCONN_INVALID)
+    {
+        DCE_DEBUG("invalid connection index");
+        dce_emit_basic_result_code(dce, DCE_RC_ERROR);
+        return DCE_OK;
+    }
+    struct espconn* connection = ctx->connections + index;
+    espconn_disconnect(connection);
+    return DCE_OK;
+}
+
+dce_result_t SECTION_ATTR ip_handle_CIPSENDI(dce_t* dce, void* group_ctx, int kind, size_t argc, arg_t* argv)
+{
+    if (kind == DCE_TEST)
+    {
+        dce_emit_extended_result_code(dce, "+CIPSENDI=<index>,\"data_to_send\"", -1, 1);
+        return DCE_OK;
+    }
+    if (argc != 2 ||
+        argv[0].type != ARG_TYPE_NUMBER ||
+        argv[1].type != ARG_TYPE_STRING)
+    {
+        DCE_DEBUG("invalid arguments");
+        dce_emit_basic_result_code(dce, DCE_RC_ERROR);
+        return DCE_OK;
+    }
+    
+    ip_ctx_t* ctx = (ip_ctx_t*) group_ctx;
+    int index = argv[0].value.number;
+    if (index >= MAX_ESP_CONNECTIONS ||
+        ctx->connections[index].type == ESPCONN_INVALID)
+    {
+        DCE_DEBUG("invalid connection index");
+        dce_emit_basic_result_code(dce, DCE_RC_ERROR);
+        return DCE_OK;
+    }
+    struct espconn* connection = ctx->connections + index;
+    espconn_sent(connection, (uint8_t*) argv[1].value.string, strlen(argv[1].value.string));
+    dce_emit_basic_result_code(dce, DCE_RC_OK);
+    return DCE_OK;
+}
+
+
 static const command_desc_t commands[] = {
-    { "CIPSTA",      &ip_handle_CIPSTA,      DCE_PARAM | DCE_READ },
-    { "CIPAP",       &ip_handle_CIPAP,       DCE_PARAM | DCE_READ },
-    { "CIPSTAMAC",   &ip_handle_CIPSTAMAC,   DCE_PARAM | DCE_READ },
-    { "CIPAPMAC",    &ip_handle_CIPAPMAC,    DCE_PARAM | DCE_READ },
-    { "CIPRESOLVE",  &ip_handle_CIPRESOLVE,  DCE_PARAM | DCE_WRITE | DCE_TEST },
-    { "CIPCREATE",   &ip_handle_CIPCREATE,   DCE_PARAM | DCE_WRITE | DCE_TEST },
-    { "CIPCLOSE",    &ip_handle_CIPCLOSE,    DCE_PARAM | DCE_WRITE | DCE_TEST },
-    { "CIPCONNECT",  &ip_handle_CIPCONNECT,  DCE_PARAM | DCE_WRITE | DCE_TEST },
+    { "CIPSTA",         &ip_handle_CIPSTA,          DCE_PARAM | DCE_READ },
+    { "CIPAP",          &ip_handle_CIPAP,           DCE_PARAM | DCE_READ },
+    { "CIPSTAMAC",      &ip_handle_CIPSTAMAC,       DCE_PARAM | DCE_READ },
+    { "CIPAPMAC",       &ip_handle_CIPAPMAC,        DCE_PARAM | DCE_READ },
+    { "CIPRESOLVE",     &ip_handle_CIPRESOLVE,      DCE_ACTION | DCE_EXEC | DCE_TEST },
+    { "CIPCREATE",      &ip_handle_CIPCREATE,       DCE_ACTION | DCE_EXEC | DCE_TEST },
+    { "CIPCLOSE",       &ip_handle_CIPCLOSE,        DCE_ACTION | DCE_EXEC | DCE_TEST },
+    { "CIPCONNECT",     &ip_handle_CIPCONNECT,      DCE_ACTION | DCE_EXEC | DCE_TEST },
+    { "CIPDISCONNECT",  &ip_handle_CIPDISCONNECT,   DCE_ACTION | DCE_EXEC | DCE_TEST },
+    { "CIPSENDI",       &ip_handle_CIPSENDI,        DCE_ACTION | DCE_EXEC | DCE_TEST },
 };
 
 static const int ncommands = sizeof(commands) / sizeof(command_desc_t);
@@ -299,7 +467,7 @@ static const int ncommands = sizeof(commands) / sizeof(command_desc_t);
 void SECTION_ATTR dce_register_ip_commands(dce_t* dce)
 {
     static ip_ctx_t ip_ctx;
+    ip_ctx_init(&ip_ctx);
     ip_ctx.dce = dce;
-    memset(ip_ctx.connections, 0, sizeof(struct espconn) * MAX_ESP_CONNECTIONS);
     dce_register_command_group(dce, "CI", commands, ncommands, &ip_ctx);
 }
