36a8ee75d17d9070162afdeb512d248a032990a3
Ivan Grokhotkov <igrokhotkov@gmail.com>
2014-10-29 18:20:22 +0300

    Add lwip to target makefile

--- target/esp8266/target.mk
+++ target/esp8266/target.mk
@@ -1,13 +1,13 @@
-TARGET_OBJ_FILES := main.o \
-					uart.o \
-					interface_commands.o \
-					info_commands.o \
-					wifi_commands.o \
-					ip_commands.o \
-					ip_commands_info.o \
-					ip_commands_common.o \
-					ip_commands_socket.o \
-					config_store.o \
+TARGET_OBJ_FILES := 	main.o \
+			uart.o \
+			interface_commands.o \
+			info_commands.o \
+			wifi_commands.o \
+			ip_commands.o \
+			ip_commands_info.o \
+			ip_commands_common.o \
+			ip_commands_socket.o \
+			config_store.o \
 
 
 TARGET_OBJ_PATHS := $(addprefix $(TARGET_DIR)/,$(TARGET_OBJ_FILES))
@@ -25,16 +25,59 @@ SDK_BASE := ../esp_iot_sdk_v0.9.2
 
 SDK_AT_DIR := $(SDK_BASE)/examples/at
 
+LWIP_DIR := $(TARGET_DIR)/../../lwip/src
+
 SDK_DRIVER_OBJ_FILES := 
 SDK_DRIVER_OBJ_PATHS := $(addprefix $(SDK_AT_DIR)/driver/,$(SDK_DRIVER_OBJ_FILES))
 
-CPPFLAGS += -I$(XTENSA_LIBS)/include \
-			-I$(SDK_BASE)/include \
-			-I$(SDK_AT_DIR)/include
+LWIP_OBJ_FILES:= api/api_lib.o \
+		api/api_msg.o \
+		api/err.o \
+		api/netbuf.o \
+		api/netdb.o \
+		api/netifapi.o \
+		api/pppapi.o \
+		api/sockets.o \
+		api/tcpip.o \
+		core/def.o \
+		core/dhcp.o \
+		core/dns.o \
+		core/inet_chksum.o \
+		core/init.o \
+		core/mem.o \
+		core/memp.o \
+		core/netif.o \
+		core/pbuf.o \
+		core/raw.o \
+		core/stats.o \
+		core/sys.o \
+		core/tcp.o \
+		core/tcp_in.o \
+		core/tcp_out.o \
+		core/timers.o \
+		core/udp.o \
+		core/ipv4/autoip.o \
+		core/ipv4/icmp.o \
+		core/ipv4/igmp.o \
+		core/ipv4/ip4.o \
+		core/ipv4/ip4_addr.o \
+		core/ipv4/ip_frag.o \
+		netif/etharp.o \
+		netif/ethernetif.o \
+		netif/slipif.o 
 
-LDFLAGS  += -L$(XTENSA_LIBS)/lib \
-			-L$(XTENSA_LIBS)/arch/lib \
-			-L$(SDK_BASE)/lib
+LWIP_OBJ_PATHS := $(addprefix $(LWIP_DIR)/,$(LWIP_OBJ_FILES))
+
+
+CPPFLAGS += 	-I$(XTENSA_LIBS)/include \
+		-I$(SDK_BASE)/include \
+		-I$(SDK_AT_DIR)/include \
+		-I$(TARGET_DIR) \
+		-I$(LWIP_DIR)/include
+
+LDFLAGS  += 	-L$(XTENSA_LIBS)/lib \
+		-L$(XTENSA_LIBS)/arch/lib \
+		-L$(SDK_BASE)/lib
 
 CFLAGS+=-std=c99
 CPPFLAGS+=-DESP_PLATFORM=1
@@ -52,9 +95,14 @@ APP_AR:=$(BIN_DIR)/app.a
 APP_OUT:=$(BIN_DIR)/app.out
 APP_FW_1 := $(BIN_DIR)/0x00000.bin
 APP_FW_2 := $(BIN_DIR)/0x40000.bin
+LWIP_AR:=$(BIN_DIR)/lwip.a
 
+$(LWIP_AR) : $(LWIP_OBJ_PATHS)
+	$(AR) cru $@ $^
 
-$(APP_AR): $(COMMON_OBJ_PATHS) $(TARGET_OBJ_PATHS) $(SDK_DRIVER_OBJ_PATHS)
+$(LWIP_AR): | $(BIN_DIR)
+
+$(APP_AR): $(COMMON_OBJ_PATHS) $(TARGET_OBJ_PATHS) $(SDK_DRIVER_OBJ_PATHS) $(LWIP_AR)
 	$(AR) cru $@ $^
 
 $(APP_AR): | $(BIN_DIR)
@@ -74,6 +122,12 @@ all: firmware
 
 clean-driver:
 	rm -r $(SDK_DRIVER_OBJ_PATHS)
+
+clean-lwip:
+	rm -rf $(LWIP_OBJ_PATHS)
+	rm -rf $(LWIP_AR)
+
+clean: clean-lwip
 
 #clean:	clean-driver
 
